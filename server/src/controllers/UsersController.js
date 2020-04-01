b1fed9136cfe9cf68736acaf20195aae837c1ccd
pashamakhilkumarreddy <akhil.reddy@frequence.com>
2020-04-01 20:56:28 +0530

    Add Single User View and minor code fixes

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -17,9 +17,24 @@ module.exports = {
       const users = getUsers();
       const isValidUser = users.filter((user) => user.id === id);
       if (isValidUser.length) {
+        const validUser = isValidUser[0];
+        const userProperties = {
+          id: validUser.id,
+          name: validUser.name,
+          username: validUser.username,
+          email: validUser.email,
+          phone: validUser.phone,
+          website: validUser.website,
+          address: {
+            ...validUser.address,
+          },
+          company: {
+            ...validUser.company,
+          },
+        };
         res.status(200).send({
           err: false,
-          user: isValidUser,
+          user: [{ ...userProperties }],
         });
         return;
       }
